f8c111a1eb2534e2c8bfd66c55e8474b146df4ae
Landyson <landamartin220@gmail.com>
2025-12-29 17:32:32 +0100

    forgot to add customer model

---
+++ src/OrderSystem.Web/Models/Customer.cs
@@ -0,0 +1,11 @@
+namespace OrderSystem.Web.Models;
+
+public sealed class Customer
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; } = "";
+    public string LastName { get; set; } = "";
+    public string Email { get; set; } = "";
+    public string? Phone { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
